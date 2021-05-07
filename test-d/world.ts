86e41de80730ccca5a321eebe6e930b7ce88b67a
davidgoss <david@davidgoss.co>
2021-05-07 13:05:42 +0100

    extend test

--- test-d/world.ts
+++ test-d/world.ts
@@ -2,16 +2,18 @@ import { Before, World } from '../'
 
 // `this` should implicitly be of type `World` if not specified
 Before(async function () {
-  if (this.parameters.foo) {
+  if (!this.parameters.foo) {
     throw new Error('test')
   }
 })
 
-class CustomWorld extends World {}
+class CustomWorld extends World {
+  readonly foo: string = 'bar'
+}
 
-// `this` should implicitly be of type `World` if not specified
+// `this` can be overridden with custom world class
 Before(async function (this: CustomWorld) {
-  if (this.parameters.foo) {
+  if (!this.foo) {
     throw new Error('test')
   }
 })
